795862485e1c388fab9707f558691625b7502be3
ItsRudraksh <rudrakshkapoor2004@gmail.com>
2024-05-18 23:26:36 +0530

    applied react router for scrolling to sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faFacebookF,
@@ -5,6 +6,7 @@ import {
   faTwitter,
   faLinkedinIn,
 } from "@fortawesome/free-brands-svg-icons";
+
 const Header = () => {
   const navLinks = [
     { text: "Home", href: "#home" },
@@ -21,18 +23,32 @@ const Header = () => {
     { href: "https://www.skype.com/en/", icon: faSkype },
     { href: "https://www.linkedin.com/home", icon: faLinkedinIn },
   ];
+  const navigate = useNavigate();
+  const handleClick = (e, href) => {
+    e.preventDefault();
+    navigate(`${href}`);
+    const targetId = href.substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="w-full h-24 bg-[rgba(0,0,0,0.4)] px-4 flex items-center justify-between border-b-2 border-b-[#7b7b7b] fixed top-0 z-10 backdrop-blur-sm">
       <div className="w-1/5 flex items-center justify-end">
-        <a href="#home">
+        <Link
+          to={{ pathname: "/", hash: "#home" }}
+          onClick={(e) => handleClick(e, "#home")}
+        >
           <img
             src="/Images/Logo.png"
-            alt=""
+            alt="Logo"
             width={67}
             height={67}
             className="aspect-square object-cover"
           />
-        </a>
+        </Link>
       </div>
       <nav className="flex flex-row gap-12 flex-grow items-center justify-center h-full">
         <ul
@@ -44,14 +60,19 @@ const Header = () => {
               key={index}
               className="flex items-center h-full border-0 transition-all ease-in-out text-xl font-myriad-pro hover:border-b-8 border-b-[#11749e] cursor-pointer"
             >
-              <a href={link.href}>{link.text}</a>
+              <Link
+                to={{ pathname: "/", hash: link.href }}
+                onClick={(e) => handleClick(e, link.href)}
+              >
+                {link.text}
+              </Link>
             </li>
           ))}
         </ul>
         <ul id="icon-links" className="flex flex-row gap-6 text-white text-xl">
           {iconLinks.map((link, index) => (
             <li key={index}>
-              <a href={link.href} target="_blank">
+              <a href={link.href} target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={link.icon} />
               </a>
             </li>
