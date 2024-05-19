12f1e25ac2feea1478bd4ba9621af1b6f525aae8
ItsRudraksh <rudrakshkapoor2004@gmail.com>
2024-05-19 14:43:06 +0530

    fixed QuickAction

--- src/components/QuickAction.jsx
+++ src/components/QuickAction.jsx
@@ -1,7 +1,7 @@
 import { QuickActionItem } from "./Constants";
 const QuickAction = () => {
   return (
-    <div className="bg-[#11749e] flex items-center justify-center gap-24 h-64 sm:flex-col md:flex-col md:pt-5 md:pb-5 md:gap-4">
+    <div className="bg-[#11749e] flex items-center justify-center gap-24 h-64 sm:flex-col md:flex-col md:pt-5 md:pb-5 md:gap-4 sm:gap-8">
       <QuickActionItem>
         Cum sociis natoque penatibus et magnis dis parturient
         <br />
